04bdeab23731a7d03b71db2636926e27960eb707
syzer3@gmail.com <syzer3@gmail.com>
2015-07-11 17:35:43 +0200

    gol user side display pixels

--- private/src/client/app/gol/gol.controller.js
+++ private/src/client/app/gol/gol.controller.js
@@ -2,6 +2,13 @@
 
 angular.module('jsSparkUiApp')
     .controller('GolCtrl', function ($scope, $http, socket) {
+        var COLOR = "#E05C76";
+        var COLOR2 = "#FFFFFF";
+
+        var canv = document.getElementById('canvas');
+        var ctx = canv.getContext('2d');
+        ctx.fillStyle = COLOR;
+
         $scope.gols = [];
 
         $http.get('/api/gols').success(function (items) {
@@ -9,7 +16,22 @@ angular.module('jsSparkUiApp')
             socket.syncUpdates('gol', $scope.gols);
         });
 
+        $scope.$watchCollection('gols', function (newValue, oldValue) {
+            if (_.isEmpty(newValue)) {
+                return;
+            }
+            console.warn(newValue, oldValue);
+            drawCanvas(newValue);
+        });
+
         $scope.$on('$destroy', function () {
             socket.unsyncUpdates('gol');
         });
+
+        function drawCanvas(items) {
+            items.forEach(function (pix) {
+                ctx.fillRect(pix.x, pix.y, 10, 10);
+            });
+        }
+
     });
